e1699eac1b4ef34efac8a8ceb4d6b7206622de00
NasiarN <126NNiblack@frhsd.com>
2024-02-21 12:54:20 -0500

    Edited addAssignment and constructor for Student

--- Student.java
+++ Student.java
@@ -1,19 +1,27 @@
 public class Student {
 private String name;
+private List<String> grades;
 
 public Student(String name){
     this.name = name;
+    this.grades = new List<String>();
 }
 
 public void addAssignment(String assignName) {
+    double grade = 0;
     assignName = assignName + " Grade: Ungraded";
+    grades.add(assignName);
 
 }
 public void gradeAssignment(String assignment, double grade){
     assignment = assignment + " Grade: " + grade;
+    if()
 }
 public double getAssignmentGrade(String assignment){
     double grade = 0;
+
+}
+public double getOverallGrade(){
 
 }
 
