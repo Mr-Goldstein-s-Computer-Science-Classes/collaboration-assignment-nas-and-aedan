6bbd348bbc22fb72ad49a06df0bd76aeb021ee2a
NasiarN <126NNiblack@frhsd.com>
2024-02-22 13:28:19 -0500

    Finished getAssignmentGrade and getOverallGrade

--- Student.java
+++ Student.java
@@ -2,10 +2,12 @@ import java.util.*;
 public class Student {
 private String name;
 private List<String> assignments;
+private List<double> grades;
 
 public Student(String name){
     this.name = name;
     this.assignments = List<String>();
+    this.grades = List<double>();
 }
 
 
@@ -14,24 +16,37 @@ public void addAssignment(String assignName) {
     boolean graded = false;
     assignName = assignName + " Grade: Ungraded";
     assignments.add(assignName);
+    grades.add(grade);
 
 }
 public void gradeAssignment(String assignment, double grade){
    graded = true;
     assignment = assignment + " Grade: " + grade;
     assignments.add(assignment);
+    grades.add(grade);
 
 }
 public double getAssignmentGrade(String assignment){
-  double grade;
+  for(int i = 0; i <= grades.size();i++){
+      if(assignment.equalsIgnoreCase(assignments.get(i))){
+          return grades.get(i);
+      }
+  }
 
 
 
 }
 public double getOverallGrade(){
+    double overallGrade = 0;
+    for(int i = 0; i <= grades.size();i++){
+        overallGrade += grades.get(i);
+    }
+    return overallGrade;
 
 
 }
+
+
 
 
 
