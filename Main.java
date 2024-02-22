99c43e5d2c73a1ecaafff4e1d1ddc5dcda4fa5c3
aedan <113115936+FRHSDaedan@users.noreply.github.com>
2024-02-22 13:59:09 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,71 +1,63 @@
 public class Main {
     public static void main(String[] args) {
-        //Main:
-        //
-        //1. Create a Gradebook object called grades
-        //
-        //2. Create Students inside of grades
-        //
-        //	1. Tyler
-        //	2. James
-        //	3. Ethan
-        //	4. Scotty
-        //	5. David
-        //	6. Patrick
-        //
-        //3. Create Assignments:
-        //
-        //	1. Stacks And Queues Station Activity
-        //	2. Stacks and queues programs
-        //	3. Stacks and queues quiz
-        //	4. Github Exploration
-        //
-        //4. Grade Assignments:
-        //	1. Stacks and queues station
-        //		1. Tyler got a 94
-        //		2. James got a 14
-        //		3. Ethan got a 5
-        //		4. Scotty got a 100
-        //		5. David didn’t do it (0)
-        //		6. Patrick got a 79
-        //	2. Stacks and queues programs
-        //		1. Tyler got a 100
-        //		2. James got a 64
-        //		3. Ethan got a 100
-        //		4. Scotty got a 101
-        //		5. David got a 26
-        //		6. Patrick got a 99
-        //	3. Stacks and queues quiz
-        //		1. Tyler got a 100
-        //		2. James got a 100
-        //		3. Ethan got a 100
-        //		4. Scotty got a 87
-        //		5. David got a 92
-        //		6. Patrick got a 83
-        //	4. Github Exploration
-        //		1. Tyler got a 96
-        //		2. James got a 43
-        //		3. Ethan got a 100
-        //		4. Scotty got a 97
-        //		5. David got a 28
-        //		6. Patrick got a 100
-        //
+
+     Gradebook grades = new Gradebook();
+
+     //create some students
+        grades.createStudent("Tyler");
+        grades.createStudent("James");
+        grades.createStudent("Ethan");
+        grades.createStudent("Scotty");
+        grades.createStudent("David");
+        grades.createStudent("Patrick");
+
+     //create assignments
+        grades.addAssignment("Stacks and Queues Station Activity");
+        grades.addAssignment("Stacks and Queues programs");
+        grades.addAssignment("Stacks and Queues quiz");
+        grades.addAssignment("Github Exploration");
+
+     //grade assignments for snq stations
+        grades.gradeAssignment("Stacks and Queues Station Activity", "Tyler", 94);
+        grades.gradeAssignment("Stacks and Queues Station Activity", "James", 14);
+        grades.gradeAssignment("Stacks and Queues Station Activity", "Ethan", 5);
+        grades.gradeAssignment("Stacks and Queues Station Activity", "Scotty", 100);
+        grades.gradeAssignment("Stacks and Queues Station Activity", "David", 0);
+        grades.gradeAssignment("Stacks and Queues Station Activity", "Patrick", 79);
+
+     //grade assignments for snq programs
+        grades.gradeAssignment("Stacks and Queues programs", "Tyler", 100);
+        grades.gradeAssignment("Stacks and Queues programs", "James", 64);
+        grades.gradeAssignment("Stacks and Queues programs", "Ethan", 100);
+        grades.gradeAssignment("Stacks and Queues programs", "Scotty", 101);
+        grades.gradeAssignment("Stacks and Queues programs", "David", 26);
+        grades.gradeAssignment("Stacks and Queues programs", "Patrick", 99);
+
+     //grade assignments for snq quiz
+        grades.gradeAssignment("Stacks and Queues quiz", "Tyler", 100);
+        grades.gradeAssignment("Stacks and Queues quiz", "James", 100);
+        grades.gradeAssignment("Stacks and Queues quiz", "Ethan", 100);
+        grades.gradeAssignment("Stacks and Queues quiz", "Scotty", 87);
+        grades.gradeAssignment("Stacks and Queues quiz", "David", 92);
+        grades.gradeAssignment("Stacks and Queues quiz", "Patrick", 83);
+
+     //github exploration
+        grades.gradeAssignment("Github Exploration", "Tyler", 96);
+        grades.gradeAssignment("Github Exploration", "James", 43);
+        grades.gradeAssignment("Github Exploration", "Ethan", 100);
+        grades.gradeAssignment("Github Exploration", "Scotty", 97);
+        grades.gradeAssignment("Github Exploration", "David", 28);
+        grades.gradeAssignment("Github Exploration", "Patrick", 100);
+
+     //print stats for all assignments and overall grades
+        System.out.println("Average Grade for Stacks and Queues Station Activity: " + grades.getAverageGrade("Stacks and Queues Stations Activity"));
+        System.out.println("Average Grade for Stacks and Queues Station Activity: " + grades.getAverageGrade("Stacks and Queues Stations programs"));
+        System.out.println("Average Grade for Stacks and Queues Station Activity: " + grades.getAverageGrade("Stacks and Queues Stations quiz"));
+        System.out.println("Average Grade for Stacks and Queues Station Activity: " + grades.getAverageGrade("Github Exploration"));
+
         //5. Print out the stats for all assignments and overall grades
         //6. Print the grades object
         //7. Print the object of each student
-     Gradebook grades = new Gradebook();
-        //	1. Tyler
-        //	2. James
-        //	3. Ethan
-        //	4. Scotty
-        //	5. David
-        //	6. Patrick
-     //create some students
-        grades.createStudent("Tyler");
-        grades.createStudent("Tyler");
-        grades.createStudent("Tyler");
-        grades.createStudent("Tyler");
-        grades.createStudent("Tyler");
 
     }
 }
